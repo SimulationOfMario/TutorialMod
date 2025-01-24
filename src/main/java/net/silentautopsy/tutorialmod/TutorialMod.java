0300557256506fcbeb133658325d57bbaeb3ab70
Mario Cortés Herrera <imthedarkestspawn@gmail.com>
2025-01-24 01:43:04 +0100

    Ruby and Raw Ruby Item, Ruby ItemGroup

--- src/main/java/net/silentautopsy/tutorialmod/TutorialMod.java
+++ src/main/java/net/silentautopsy/tutorialmod/TutorialMod.java
@@ -2,24 +2,20 @@ package net.silentautopsy.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.silentautopsy.tutorialmod.items.ModItemGroups;
+import net.silentautopsy.tutorialmod.items.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer
 {
 	public static final String MOD_ID = "tutorialmod";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+	public void onInitialize()
+	{
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
 	}
 }
